da9d12fb065a28a7d9afe5ef8ab801fc99ea44cf
Boni Garcia <boni.garcia@uc3m.es>
2022-09-01 18:06:03 +0200

    Include tests for os_arch module

--- os_arch/src/main.rs
+++ os_arch/src/main.rs
@@ -1,9 +1,27 @@
 use std::env;
 
 fn main() {
-    let os = env::consts::OS.to_string();
-    let arch = env::consts::ARCH.to_string();
+    let os = my_os();
+    let arch = my_arch();
 
     println!("os {}", os);
     println!("arch {}", arch);
+}
+
+fn my_arch() -> String {
+    env::consts::ARCH.to_string()
+}
+
+fn my_os() -> String {
+    env::consts::OS.to_string()
+}
+
+#[test]
+fn test_os() {
+    assert!( ["linux", "windows", "macos"].contains(&&*my_os()));
+}
+
+#[test]
+fn test_arch() {
+    assert_eq!("x86_64", my_arch());
 }
