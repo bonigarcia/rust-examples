212527b9fabe0533d89d4222a8f24f7d3a1bbd37
Boni Garcia <boni.garcia@uc3m.es>
2022-09-03 17:41:16 +0200

    Include test in hello_world app

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn test_hello() {
+    let mut cmd = assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.assert().success().code(0).stdout("Hello, world!\n");
+}
