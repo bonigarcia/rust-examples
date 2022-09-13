a8a8ba963cd4563c56a6342184793184b6ce9755
Boni Garcia <boni.garcia@uc3m.es>
2022-09-13 21:51:04 +0200

    Enhance logging in clap derive app

--- clap_derive/src/main.rs
+++ clap_derive/src/main.rs
@@ -1,13 +1,13 @@
 use clap::Parser;
+use log::LevelFilter::{Debug, Info, Trace};
+use std::io::Write;
+use env_logger::fmt::Color;
+use log::Level;
 
 /// Simple program using --debug and --trace
 #[derive(Parser, Debug)]
 #[clap(version, about, long_about = None)]
 struct Cli {
-    /// Name of the person to greet
-    #[clap(short, long, value_parser)]
-    name: String,
-
     /// Display DEBUG messages
     #[clap(short, long)]
     debug: bool,
@@ -20,18 +20,32 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
 
-    println!("Hello {}!", cli.name);
-
     let mut filter = match cli.debug {
-        true => log::LevelFilter::Debug,
-        false => log::LevelFilter::Info,
+        true => Debug,
+        false => Info,
     };
     if cli.trace {
-        filter = log::LevelFilter::Trace
+        filter = Trace
     }
 
     env_logger::Builder::new()
         .filter_level(filter)
+        .format(|buf, record| {
+            let mut level_style = buf.style();
+            match record.level() {
+                Level::Trace => level_style.set_color(Color::Cyan),
+                Level::Debug => level_style.set_color(Color::Blue),
+                Level::Info => level_style.set_color(Color::Green),
+                Level::Warn => level_style.set_color(Color::Yellow),
+                Level::Error => level_style.set_color(Color::Red).set_bold(true),
+            };
+            writeln!(
+                buf,
+                "{}\t{}",
+                level_style.value(record.level()),
+                record.args()
+            )
+        })
         .init();
 
     log::error!("Error message");
