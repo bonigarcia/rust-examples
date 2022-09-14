7f87765b820e0a033920bd24eb7273ff7ba46bb7
Boni Garcia <boni.garcia@uc3m.es>
2022-09-14 10:01:41 +0200

    Include basic example using reqwest

---
+++ reqwest/src/main.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    println!("{}", resp.get("origin").unwrap());
+    Ok(())
+}
