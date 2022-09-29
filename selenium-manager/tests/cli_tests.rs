d46f861eafe4fd8e48957ecb1db9b4922c809123
Boni Garcia <boni.garcia@uc3m.es>
2022-09-29 10:41:10 +0200

    Rename CLI tests file

--- selenium-manager/tests/cli_tests.rs
+++ selenium-manager/tests/cli_tests.rs
@@ -6,7 +6,7 @@ use rstest::rstest;
 #[case("chrome", "", "")]
 #[case("chrome", "105", "105.0.5195.52")]
 #[case("chrome", "106", "106.0.5249.61")]
-fn test_browser_manager(#[case] browser: String, #[case] browser_version: String, #[case] driver_version: String) {
+fn cli_test(#[case] browser: String, #[case] browser_version: String, #[case] driver_version: String) {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     cmd.args(["--browser", &browser, "--version", &browser_version])
         .assert()
@@ -25,5 +25,4 @@ fn test_browser_manager(#[case] browser: String, #[case] browser_version: String
     if !browser_version.is_empty() {
         assert!(output.contains(&driver_version));
     }
-
 }
