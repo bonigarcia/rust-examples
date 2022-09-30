6a4ed3d57c852eeb94c4bc6725934b778b6b9808
Boni Garcia <boni.garcia@uc3m.es>
2022-09-30 11:53:21 +0200

    Enhance JSON demo app

--- json/src/main.rs
+++ json/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::Read;
 use std::ops::Add;
 use std::time::Duration;
+use std::fs::write;
 
 use serde::{Deserialize, Serialize};
 use time::OffsetDateTime;
@@ -64,7 +65,7 @@ fn main() {
 
     // Write to file
     let filename = "out.json";
-    std::fs::write(&filename, serde_json::to_string_pretty(&metadata).unwrap()).unwrap();
+    write(&filename, serde_json::to_string_pretty(&metadata).unwrap()).unwrap();
 
     // Read file
     let mut file = File::open(&filename).unwrap();
@@ -74,5 +75,28 @@ fn main() {
 
     println!("{} {} {}", metadata2.browsers[0].browser_name, metadata2.browsers[0].browser_version, metadata2.browsers[0].browser_version_ttl);
     println!("{} {} {}", metadata2.browsers[0].driver_name, metadata2.browsers[0].driver_version, metadata2.browsers[0].driver_version_ttl);
+
+    // Metadata #3
+    let browser_time = OffsetDateTime::from_unix_timestamp(now.unix_timestamp() + (TTL_BROWSERS as i64)).unwrap();
+    let driver_time = OffsetDateTime::from_unix_timestamp(now.unix_timestamp() + (TTL_DRIVERS as i64)).unwrap();
+
+    let mut metadata3 = Metadata {
+        browsers: Vec::new()
+    };
+    let chrome: Browser = Browser {
+        browser_name: "chrome".to_string(),
+        browser_version: "107".to_string(),
+        browser_version_ttl: browser_time,
+        driver_name: "chromedriver".to_string(),
+        driver_version: "107.xxxx".to_string(),
+        driver_version_ttl: driver_time
+    };
+    metadata3.browsers.push(chrome);
+
+    let filename2 = "out2.json";
+    write(&filename2, serde_json::to_string_pretty(&metadata3).unwrap()).unwrap();
+
+    println!("{} {} {}", metadata3.browsers[0].browser_name, metadata3.browsers[0].browser_version, metadata3.browsers[0].browser_version_ttl);
+    println!("{} {} {}", metadata3.browsers[0].driver_name, metadata3.browsers[0].driver_version, metadata3.browsers[0].driver_version_ttl);
 
 }
