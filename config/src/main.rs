9744d60bcb9b7c738ca91650192c84d6d77ddeca
Boni Garcia <boni.garcia@uc3m.es>
2022-12-05 10:50:37 +0100

    Simplify config example

--- config/src/main.rs
+++ config/src/main.rs
@@ -1,5 +1,3 @@
-use std::env::consts::{ARCH, OS};
-
 const BROWSER_NAME: &str = "chrome";
 const DRIVER_NAME: &str = "chromedriver";
 
@@ -38,17 +36,13 @@ impl SeleniumManager for ChromeManager {
 pub struct ManagerConfig {
     pub browser_version: String,
     pub driver_version: String,
-    pub os: String,
-    pub arch: String,
 }
 
 impl ManagerConfig {
     pub fn default() -> ManagerConfig {
         ManagerConfig {
-            browser_version: "AAA".to_string(),
-            driver_version: "DDD".to_string(),
-            os: OS.to_string(),
-            arch: ARCH.to_string(),
+            browser_version: "b1".to_string(),
+            driver_version: "d1".to_string(),
         }
     }
 }
@@ -62,12 +56,12 @@ fn main() {
     println!("The default driver version is {driver_version}");
 
     let mut new_config = ManagerConfig::default();
-    new_config.browser_version = "111".to_string();
-    new_config.driver_version = "222".to_string();
+    new_config.browser_version = "b2".to_string();
+    new_config.driver_version = "d2".to_string();
 
     manager.set_config(new_config);
     let new_browser_version = manager.get_config().browser_version.as_str();
     let new_driver_version = manager.get_config().driver_version.as_str();
-    println!("The default browser version is {new_browser_version}");
-    println!("The default browser driver is {new_driver_version}");
+    println!("The new browser version is {new_browser_version}");
+    println!("The new browser driver is {new_driver_version}");
 }
