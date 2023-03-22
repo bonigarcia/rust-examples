cebddd1091694bbc145b5dc14dee8a9df92284cf
Boni Garcia <boni.garcia@uc3m.es>
2023-03-22 16:50:50 +0100

    Enhance toml config example

--- toml/src/main.rs
+++ toml/src/main.rs
@@ -1,15 +1,32 @@
 use std::error::Error;
 use std::fs::read_to_string;
 use std::path::Path;
-use toml::Table;
+use toml::{Table, Value};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let config_path = Path::new("config.toml");
-    let config: Table = read_to_string(config_path)?.parse()?;
-
+    let config = get_config()?;
     println!("{:?}", config);
     let browser = config["browser"].as_str().unwrap();
     println!("browser: {}", browser);
 
+    // -----
+
+    let value = config["browser"].to_owned();
+    let content = StringKey(value);
+    println!("browser: {}", content.get_value());
+
     Ok(())
+}
+
+struct StringKey(Value);
+
+impl StringKey {
+    fn get_value(&self) -> &str {
+        self.0.as_str().unwrap()
+    }
+}
+
+fn get_config() -> Result<Table, Box<dyn Error>> {
+    let config_path = Path::new("config.toml");
+    Ok(read_to_string(config_path)?.parse()?)
 }
