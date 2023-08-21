ccac9dbc8594f3938836b93a0bef300e0be57aa6
Boni Garcia <boni.garcia@uc3m.es>
2023-08-21 15:25:26 +0200

    Include example using bzip2-rs

---
+++ bzip2/src/main.rs
@@ -0,0 +1,17 @@
+use std::error::Error;
+use std::fs::File;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // This file is downloaded from: https://ftp.mozilla.org/pub/firefox/releases/89.0.2/linux-x86_64/en-US/firefox-89.0.2.tar.bz2
+    let input = File::open("/home/boni/Downloads/firefox-89.0.2.tar.bz2")?; // this fails
+
+    // let input = File::open("/home/boni/Downloads/firefox-90.0.2.tar.bz2")?; // This works
+    // This file is downloaded from: https://ftp.mozilla.org/pub/firefox/releases/90.0.2/linux-x86_64/en-US/firefox-90.0.2.tar.bz2
+
+    let mut output = File::create("/home/boni/Downloads/output")?;
+    let mut decoder = bzip2_rs::DecoderReader::new(input);
+    std::io::copy(&mut decoder, &mut output)?;
+
+    Ok(())
+}
+
