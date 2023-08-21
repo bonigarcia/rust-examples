ca1cbb7f537b3d03d0527028f5a71707755bc348
Boni Garcia <boni.garcia@uc3m.es>
2023-08-21 15:27:44 +0200

    Include error message in bzip2 example

--- bzip2/src/main.rs
+++ bzip2/src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // This file is downloaded from: https://ftp.mozilla.org/pub/firefox/releases/89.0.2/linux-x86_64/en-US/firefox-89.0.2.tar.bz2
-    let input = File::open("/home/boni/Downloads/firefox-89.0.2.tar.bz2")?; // this fails
+    let input = File::open("/home/boni/Downloads/firefox-89.0.2.tar.bz2")?; // This fails: Error: Custom { kind: Other, error: BlockError { reason: "huffman bitstream truncated" } }
 
     // let input = File::open("/home/boni/Downloads/firefox-90.0.2.tar.bz2")?; // This works
     // This file is downloaded from: https://ftp.mozilla.org/pub/firefox/releases/90.0.2/linux-x86_64/en-US/firefox-90.0.2.tar.bz2
@@ -14,4 +14,3 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
