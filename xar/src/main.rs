6ca28646a833154972b3e9f15f30cc6a023e3db8
Boni Garcia <boni.garcia@uc3m.es>
2023-11-06 18:22:01 +0100

    Update xar example

--- xar/src/main.rs
+++ xar/src/main.rs
@@ -1,15 +1,21 @@
-use std::error::Error;
-use std::io::Cursor;
 use apple_xar::reader::XarReader;
+use std::error::Error;
+use std::fs::File;
+use tar::Archive;
+use flate2::read::GzDecoder;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let source = r#"C:\Users\boni\Downloads\MicrosoftEdge-116.0.1938.76.pkg"#;
+    let source = r#"C:\Users\boni\Downloads\MicrosoftEdge-119.0.2151.46.pkg"#;
     let target = r#"C:\Users\boni\Downloads\extract"#;
 
-    let cursor = Cursor::new(source);
-    let mut reader = XarReader::new(cursor)?;
+    let mut reader = XarReader::new(File::open(source)?)?;
     reader.unpack(target)?;
+
+    // let payload = File::open(r#"C:\Users\boni\Downloads\extract\MicrosoftEdge-119.0.2151.46.pkg\Payload"#)?;
+    // let tar = GzDecoder::new(&payload);
+    //
+    // let mut archive = Archive::new(tar);
+    // archive.unpack(target)?;
 
     Ok(())
 }
-
