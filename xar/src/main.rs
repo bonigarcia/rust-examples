dc87fac10383522c141e98802606500c633488d0
Boni Garcia <boni.garcia@uc3m.es>
2024-03-26 17:12:47 +0100

    Update demo to extract PKG files

--- xar/src/main.rs
+++ xar/src/main.rs
@@ -4,23 +4,22 @@ use std::fs::File;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Source file obtained from
-    // https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/a989b093-c982-4d31-95d1-2c439f49b7e7/MicrosoftEdge-116.0.1938.76.pkg
-    let source = r#"C:\Users\boni\Downloads\MicrosoftEdge-116.0.1938.76.pkg"#;
+    // https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/61b13da3-c921-482a-9166-743689310b71/MicrosoftEdge-122.0.2365.92.pkg
+    let source = r#"C:\Users\boni\Downloads\MicrosoftEdge-122.0.2365.92.pkg"#;
+    // let target = "/home/boni/Downloads/extract";
+    // let target_path = Path::new(target);
 
     let mut reader = PkgReader::new(File::open(source)?)?;
     let packages = reader.component_packages()?;
     println!("Number of components: {}", packages.len());
     for package in packages.iter() {
-        let package_info = package.package_info();
-        println!("Package info: {:?}", package_info);
-        let payload = &package_info.unwrap().payload;
-        println!("Payload: {:?}", payload.clone().unwrap());
-    }
-
-    let resolve = reader.resolve_component("Payload")?;
-    if resolve.is_some() {
-        let component = resolve.unwrap();
-        println!("Resolved payload: {:?}", component.package_info());
+        if let Some(cpio_reader) = package.payload_reader()? {
+            for entry in cpio_reader {
+                let e = entry?;
+                let name = e.name();
+                println!("---> {}", name);
+            }
+        }
     }
 
     Ok(())
